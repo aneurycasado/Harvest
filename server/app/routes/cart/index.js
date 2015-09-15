cf937e01b6e84c277e0c827dcccd812e65efe46d
Ryan Dwyer <ryankdwyer@gmail.com>
2015-09-15 17:50:56 -0400

    Finalized cart functionality except checkout...

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -5,15 +5,16 @@ var Cart = mongoose.model('Cart');
 var Product = mongoose.model('Product');
 module.exports = router;
 
-	
-router.get("/:userID", function(req,res,next){
-	Cart.findOne({user:req.params.userID}).then(function(cart){
-		console.log("Got Cart ", cart);
-		res.json(cart);
-	}).then(null,function(error){
-		console.error(error);
-		next(error);
-	});
+
+router.get("/:userID", function (req, res, next) {
+    Cart.findOne({
+        user: req.params.userID
+    }).populate('contents').then(function (cart) {
+        res.json(cart);
+    }).then(null, function (error) {
+        console.error(error);
+        next(error);
+    });
 });
 
 router.put("/:productID", function (req, res, next) {
@@ -48,5 +49,54 @@ router.put("/:productID", function (req, res, next) {
         }).then(null, function (error) {
             console.log(error);
             next(error);
+        });
+});
+
+router.put('/:cartID/:productID', function (req, res, next) {
+    var cartID = req.params.cartID;
+    var productID = req.params.productID;
+    if (cartID === 'update') next();
+    else {
+        Cart.findOne({
+                _id: cartID
+            })
+            .then(function (cart) {
+                // Extract out to a function...when Aneury is ready
+                var newCartContents = [];
+                for (var i = 0; i < cart.contents.length; i++) {
+                    var product = cart.contents[i].toString();
+                    if (product !== productID) {
+                        newCartContents.push(product);
+                    }
+                }
+                cart.contents = newCartContents;
+                cart.save()
+                    .then(function (savedCart) {
+                        res.json(savedCart);
+                    });
+            }).then(null, function (error) {
+                next(error);
+            });
+    }
+});
+
+router.put('/update/:cartID', function (req, res, next) {
+    var cartID = req.params.cartID;
+    res.json(req.body);
+    Cart.findOne({
+            _id: cartID
+        })
+        .then(function (cart) {
+            var newContents = [];
+            req.body.updatedCartContents.forEach(function (uniqueProduct) {
+                for (var i = 0; i < uniqueProduct.cartQuantity; i++) {
+                    newContents.push(uniqueProduct._id);
+                }
+            });
+            cart.contents = newContents;
+            cart.save()
+                .then(function (savedCart) {
+                    res.json(savedCart);
+                });
         });
 });
