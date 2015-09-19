cf1588cf23f7339e8dc40cc8f536c91697ecb680
steve <spells24@gmail.com>
2015-09-19 17:20:53 -0400

    added post hook to review model to update product

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -14,19 +14,19 @@ var schema = new mongoose.Schema({
 
 mongoose.model('Review', schema);
 
-// schema.post('save', function(doc){
-// 	return Product.findOne({_id: doc.product}).exec()
-// 	.then(function(prod){
-// 		var liked = doc.thumbsUp ? 1 : 0;
-// 		console.log("before add: ", prod.numReviews);
-// 		prod.percentageLiked = 
-// 		(Math.round(prod.percentageLiked*prod.numReviews)+liked) / (prod.numReviews+1);
-// 		prod.numReviews = prod.numReviews+1;
-// 		console.log("after add: ", prod.numReviews);
-// 		return prod.save();
-// 	})
-// 	.then(function(prod){
-// 		console.log("after save: ", prod.numReviews);
-// 		return doc;
-// 	});
-// });
+schema.post('save', function(doc){
+	return Product.findOne({_id: doc.product}).exec()
+	.then(function(prod){
+		var liked = doc.thumbsUp ? 1 : 0;
+		console.log("before add: ", prod.numReviews);
+		prod.percentageLiked = 
+		(Math.round(prod.percentageLiked*prod.numReviews)+liked) / (prod.numReviews+1);
+		prod.numReviews = prod.numReviews+1;
+		console.log("after add: ", prod.numReviews);
+		return prod.save();
+	})
+	.then(function(prod){
+		console.log("after save: ", prod.numReviews);
+		return doc;
+	});
+});
