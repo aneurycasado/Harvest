e384973d7fe9af136c20f90cff6d99742440cbaa
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-09-19 22:50:09 -0400

    Review form created

--- browser/js/product/product.controller.js
+++ browser/js/product/product.controller.js
@@ -3,6 +3,7 @@ app.controller('ProductCtrl', function ($scope, $state, reviews, product, CartFa
   $scope.reviews = reviews;
   $scope.loggedIn = true;
   $scope.showCreateReviewForm = false;
+  $scope.newReview = {};
   $scope.isAuthenticated = AuthService.isAuthenticated();
   $scope.photos = [
     $scope.product.photo,$scope.product.photo2,
@@ -15,7 +16,19 @@ app.controller('ProductCtrl', function ($scope, $state, reviews, product, CartFa
   $scope.revealForm = function () {
     $scope.showCreateReviewForm = !$scope.showCreateReviewForm;
   };
-
+  $scope.setThumbStyle = function(boolean){
+    console.log(typeof boolean);
+    if(boolean){
+      console.log("Green");
+      $scope.newReview.thumbsUp = true;
+      $scope.thumbsUp = "greenThumb";
+      $scope.thumbsDown = "";
+    }else if(!boolean){
+      $scope.newReview.thumbsUp = false;
+      $scope.thumbsDown = "redThumb";
+      $scope.thumbsUp = "";
+    }
+  }
   $scope.createReview = function() {
     $scope.newReview.product = $scope.product;
     ReviewFactory.createReview($scope.product,$scope.newReview).then(function(review){
