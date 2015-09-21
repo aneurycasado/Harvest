380e4f6926e81ea905b2ee49d4a29cb275a36360
Ryan Dwyer <ryankdwyer@gmail.com>
2015-09-21 19:15:09 -0400

    Added drag and drop functionality to cart and home page

--- browser/js/common/directives/promoAccordion/promoAccordionCtrl.js
+++ browser/js/common/directives/promoAccordion/promoAccordionCtrl.js
@@ -1,26 +1,26 @@
 app.controller('PromoAccordionCtrl', function ($scope, $state, PromoFactory) {
-	$scope.oneAtATime = true;
-	$scope.showNewPromo = false;
+    $scope.oneAtATime = true;
+    $scope.showNewPromo = false;
 
-	$scope.updatePromo = function (promo) {
-	    PromoFactory.updatePromo(promo);
-	    $state.reload();
-	};
+    $scope.updatePromo = function (promo) {
+        PromoFactory.updatePromo(promo);
+        $state.reload();
+    };
 
-	$scope.showNewPromoForm = function () {
-	    $scope.showNewPromo = true;
-	};
+    $scope.showNewPromoForm = function () {
+        $scope.showNewPromo = true;
+    };
 
-	$scope.createPromo = function (promo) {
-	    PromoFactory.createPromo(promo);
-	};
+    $scope.createPromo = function (promo) {
+        PromoFactory.createPromo(promo);
+    };
 
-	$scope.deletePromo = function (promo) {
-	    PromoFactory.deletePromo(promo);
-	};
+    $scope.deletePromo = function (promo) {
+        PromoFactory.deletePromo(promo);
+    };
 
-	$scope.status = {
-	    isFirstOpen: true,
-	    isFirstDisabled: false
-	};
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
 });
