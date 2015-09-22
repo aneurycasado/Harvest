fb8856f39180e14aa2cdde69a4b3b0554ae446c6
steve <spells24@gmail.com>
2015-09-22 00:21:23 -0400

    almost completed navigation bar, only categories left

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,5 +1,40 @@
-app.controller('HomeCtrl', function ($scope, products, $state, CartFactory) {
+app.controller('HomeCtrl', function ($scope, products, $state, CartFactory, ProductFactory) {
     $scope.products = products;
+    $scope.minPrice = ProductFactory.minPrice(products);
+    $scope.maxPrice = ProductFactory.maxPrice(products);
+
+    $scope.priceSlider = {
+        floor: ProductFactory.minPrice(products),
+        ceil: ProductFactory.maxPrice(products),
+        min: $scope.minPrice,
+        max: $scope.maxPrice
+    };
+    $scope.reviewSlider = {
+        floor: 0,
+        ceil: 100,
+        min: 0,
+        max: 100
+    };
+    $scope.resetPrice = function(){
+        $scope.priceSlider.min = $scope.minPrice;
+        $scope.priceSlider.max = $scope.maxPrice;
+    };
+    $scope.resetReview = function(){
+        $scope.reviewSlider.min = 0;
+        $scope.reviewSlider.max = 100;
+    };
+    $scope.clearAll = function(){
+        $scope.resetReview();
+        $scope.resetPrice();
+    };
+
+    $scope.translate = function(val){
+        return '$' + val;
+    };
+    $scope.translate2 = function(val){
+        return val+"%";
+    };
+
     $scope.goToProduct = function (product) {
         var productID = product._id;
         $state.go('product', {
@@ -14,5 +49,30 @@ app.controller('HomeCtrl', function ($scope, products, $state, CartFactory) {
     };
     $scope.addToCart = function (product) {
       CartFactory.addToCart(product);
+    };
+    $scope.clearFilters = function () {
+        $scope.searchInput = "";
+    };
+});
+app.filter('pricefilt', function(){
+    return function(input, minPrice, maxPrice){
+        var out = [];
+        input.forEach(function(e){
+            if(e.price>=minPrice && e.price<=maxPrice){
+                out.push(e);
+            }
+        });
+        return out;
+    };
+});
+app.filter('reviewfilt', function(){
+    return function(input, minPerc, maxPerc){
+        var out = [];
+        input.forEach(function(e){
+            if(e.percentageLiked*100>=minPerc && e.percentageLiked*100<=maxPerc){
+                out.push(e);
+            }
+        });
+        return out;
     };
 });
--- browser/js/home/home.state.js
+++ browser/js/home/home.state.js
@@ -1,10 +1,33 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html',
+        templateUrl: 'js/home/home2.html',
         resolve: {
             products: ProductFactory => ProductFactory.getAll()
         },
         controller: 'HomeCtrl'
     });
 });
+
+app.directive('sticky', function($window) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      angular.element($window).ready(function() {
+        var botCutoff = $window.pageYOffset - 220,
+            topPadding = 85;
+        console.log($window)
+        angular.element($window).on('scroll',(function() {
+          if ($window.scrollY > offset) {
+          	console.log($window.scrollY);
+          } else {
+            element.stop().animate({
+              marginTop: 0
+            });
+          }
+        }));
+      });
+    }
+  };
+});
+
