3af1b6a339f42775729ea98f99fa965415b8bfde
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-09-21 21:41:55 -0400

    basic farms implemented

--- browser/js/farmHome/farmHome.controller.js
+++ browser/js/farmHome/farmHome.controller.js
@@ -4,13 +4,4 @@ app.controller('FarmHomeCtrl', function ($scope, $state, farms) {
   $scope.goToFarm = function(farm){
     $state.go("farm", {"name": farm.user.name, "id": farm._id});
   }
-  $scope.goToProduct = function (product) {
-      var productID = product._id;
-      $state.go('productDetail', {
-          id: productID
-      });
-  };
-  $scope.addToCart = function (product) {
-    CartFactory.addToCart(product);
-  };
 });
