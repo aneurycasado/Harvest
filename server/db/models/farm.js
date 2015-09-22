91ca0e23faa7434368c69a2d4bb388f97bb60f88
Ryan Dwyer <ryankdwyer@gmail.com>
2015-09-22 18:36:13 -0400

    Improved seed file, improved css

--- server/db/models/farm.js
+++ server/db/models/farm.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose');
 
 var farmSchema = new mongoose.Schema({
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}]
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }],
+    photo: {
+        type: String
+    }
 });
 
 mongoose.model('Farm', farmSchema);
--- server/db/models/review.js
+++ server/db/models/review.js
@@ -17,15 +17,12 @@ schema.post('save', function(doc){
 	return Product.findOne({_id: doc.product}).exec()
 	.then(function(prod){
 		var liked = doc.thumbsUp ? 1 : 0;
-		console.log("before add: ", prod.numReviews);
 		prod.percentageLiked = 
 		(Math.round(prod.percentageLiked*prod.numReviews)+liked) / (prod.numReviews+1);
 		prod.numReviews = prod.numReviews+1;
-		console.log("after add: ", prod.numReviews);
 		return prod.save();
 	})
 	.then(function(prod){
-		console.log("after save: ", prod.numReviews);
 		return doc;
 	});
 });
