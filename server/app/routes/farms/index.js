cb32ae49c1ce7725907f914d3904fd4498e1f133
Ryan Dwyer <ryankdwyer@gmail.com>
2015-09-22 18:52:14 -0400

    Took out console log on farm routes

--- server/app/routes/farms/index.js
+++ server/app/routes/farms/index.js
@@ -6,7 +6,6 @@ module.exports = router;
 router.get("/", function(req,res,next){
   Farm.find({}).populate("user products")
   .then(function(farms){
-    console.log("Farms ", farms);
     res.json(farms);
   })
   .then(null,next);
@@ -15,7 +14,6 @@ router.get("/", function(req,res,next){
 router.get("/:id", function(req,res,next){
   Farm.findOne({_id:req.params.id}).populate("user products")
   .then(function(farm){
-    console.log("Farm ", farm);
     res.json(farm);
   })
   .then(null,next);
